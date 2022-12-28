aacfaa9521753df6cd0d8067df1fcd9e4eebec9f
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-28 17:02:39 -0300

    Feat: Implementando método para obter topicos pelo id e, para isso, criando os DTOs TopicoDdetalhesDto e RespostaDto

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.TopicoDetalhesDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -47,6 +49,12 @@ public class TopicosController {
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping("/{id}")
+	public TopicoDetalhesDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getReferenceById(id);
+		return new TopicoDetalhesDto(topico);
 	}
 
 }
